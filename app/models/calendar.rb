7d13925879fd7429ad3d0f266dc508744826eb05
Marco Roth <marco.roth@intergga.ch>
2024-09-01 00:51:49 +0200

    Use `America/Toronto` timezone and add test for it

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -9,13 +9,13 @@ class Calendar
     calendar = Icalendar::Calendar.new
 
     calendar.timezone do |timezone|
-      timezone.tzid = "UTC"
+      timezone.tzid = timezone_identifier
     end
 
     @events.each do |event|
       calendar.event do |e|
-        e.dtstart     = Icalendar::Values::DateTime.new(event.start_at.utc)
-        e.dtend       = Icalendar::Values::DateTime.new((event.start_at + 2.hours).utc)
+        e.dtstart     = ical_time(event.start_at)
+        e.dtend       = ical_time(event.start_at + 2.hours)
         e.summary     = event.name
         e.description = event.presentation
         e.location    = "Toronto, Canada" # TODO: Use event.location, but it's too long for the calendar with all the details and instructions
@@ -28,5 +28,23 @@ class Calendar
 
   def to_ical
     publish.to_ical
+  end
+
+  private
+
+  def timezone_identifier
+    "America/Toronto"
+  end
+
+  def timezone
+    @timezone ||= TZInfo::Timezone.get(timezone_identifier)
+  end
+
+  def ical_timezone
+    timezone.ical_timezone(timezone.now)
+  end
+
+  def ical_time(datetime)
+    Icalendar::Values::DateTime.new(datetime, tzid: timezone_identifier)
   end
 end
---
+++ test/controllers/events_controller_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class EventsControllerTest < ActionController::TestCase
+  def setup
+    begining = Date.parse("2024-01-01").beginning_of_year
+    dates = 12.times.map { |i| begining + i.months + rand(1..10).days }
+    @events = dates.map { |date| Event.create!(start_at: date, name: "Toronto.rb #{date.strftime("%B %Y")}", location: "Toronto, Canada", presentation: "Presentation #{date}") }
+  end
+
+  test "all events in ics format" do
+    get :all, format: :ics
+    assert_response :success
+    assert_equal "text/calendar; charset=utf-8", response.content_type
+
+    calendar = Icalendar::Calendar.parse(response.body).first
+    events = calendar.events
+
+    assert_equal 12, events.count
+    assert_equal @events.map(&:start_at).map(&:to_date).sort, events.map(&:dtstart).map(&:to_date).sort
+    assert_equal @events.map(&:start_at).map { |date| "Toronto.rb #{date.strftime("%B %Y")}" }, events.map(&:summary)
+    assert_equal @events.map(&:id).map { |id| "https://toronto-ruby.com/events/#{id}" }, events.map(&:url).map(&:to_s)
+    assert_equal ["Toronto, Canada"], events.map(&:location).uniq
+    assert_equal ["EST", "EST", "EST", "EDT", "EDT", "EDT", "EDT", "EDT", "EDT", "EDT", "EST", "EST"], events.map(&:dtstart).map(&:zone)
+  end
+end
