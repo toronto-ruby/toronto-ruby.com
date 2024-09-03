6a787479ae1273f35824c62c5669023c4ef989db
Marco Roth <marco.roth@intergga.ch>
2024-09-03 22:59:24 +0200

    Update tests

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -5,7 +5,7 @@ class EventsControllerTest < ActionController::TestCase
     Event.destroy_all
     begining = Date.parse("2024-01-01").beginning_of_year
     dates = 12.times.map { |i| begining + i.months + rand(1..10).days }
-    @events = dates.map { |date| Event.create!(start_at: date, name: "Toronto.rb #{date.strftime("%B %Y")}", location: "Toronto, Canada", presentation: "Presentation #{date}") }
+    @events = dates.map { |date| Event.create!(start_at: date, name: "Event #{date.strftime("%B %Y")}", location: "Some Office", city: "Toronto, Canada", presentation: "Presentation #{date}") }
   end
 
   test "all events in ics format" do
@@ -18,8 +18,8 @@ class EventsControllerTest < ActionController::TestCase
 
     assert_equal 12, events.count
     assert_equal @events.map(&:start_at).map(&:to_date).sort, events.map(&:dtstart).map(&:to_date).sort
-    assert_equal @events.map(&:start_at).map { |date| "Toronto.rb #{date.strftime("%B %Y")}" }, events.map(&:summary)
-    assert_equal @events.map(&:id).map { |id| "https://toronto-ruby.com/events/#{id}" }, events.map(&:url).map(&:to_s)
+    assert_equal @events.map(&:start_at).map { |date| "Toronto Ruby - Event #{date.strftime("%B %Y")}" }, events.map(&:summary)
+    assert_equal @events.map(&:slug).map { |slug| "http://test-env.toronto-ruby.com:3000/events/#{slug}" }, events.map(&:url).map(&:to_s)
     assert_equal ["Toronto, Canada"], events.map(&:location).uniq
     assert_equal ["EST", "EST", "EST", "EDT", "EDT", "EDT", "EDT", "EDT", "EDT", "EDT", "EST", "EST"], events.map(&:dtstart).map(&:zone)
   end
