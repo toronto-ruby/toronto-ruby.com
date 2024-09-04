12025b6bb29384493faafb8a7952cd345acf6fa3
Marco Roth <marco.roth@intergga.ch>
2024-09-04 16:29:47 +0200

    Fix tests

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -4,7 +4,7 @@ class EventsControllerTest < ActionController::TestCase
   def setup
     Event.destroy_all
     begining = Date.parse("2024-01-01").beginning_of_year
-    dates = 12.times.map { |i| begining + i.months + rand(1..10).days }
+    dates = 12.times.map { |i| begining + i.months }
     @events = dates.map { |date| Event.create!(start_at: date, name: "Event #{date.strftime("%B %Y")}", location: "Some Office", city: "Toronto, Canada", presentation: "Presentation #{date}", status: :published) }
   end
 
@@ -21,7 +21,7 @@ class EventsControllerTest < ActionController::TestCase
     assert_equal @events.map(&:start_at).map { |date| "Toronto Ruby - Event #{date.strftime("%B %Y")}" }, events.map(&:summary)
     assert_equal @events.map(&:slug).map { |slug| "http://test-env.toronto-ruby.com:3000/events/#{slug}" }, events.map(&:url).map(&:to_s)
     assert_equal ["Toronto, Canada"], events.map(&:location).uniq
-    assert_equal ["EST", "EST", "EST", "EDT", "EDT", "EDT", "EDT", "EDT", "EDT", "EDT", "EST", "EST"], events.map(&:dtstart).map(&:zone)
+    assert_equal ["EST", "EST", "EST", "EDT", "EDT", "EDT", "EDT", "EDT", "EDT", "EDT", "EDT", "EST"], events.map(&:dtstart).map(&:zone)
   end
 
   test "shouldn't show draft events" do
