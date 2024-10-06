c9fe44648f9743b7a354423a974725599ac82d54
crespire <hello@crespire.dev>
2024-10-06 11:47:18 -0400

    Updates to admin controller, views and testing

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -58,7 +58,7 @@ module Admin
 
     # Use callbacks to share common setup or constraints between actions.
     def set_event
-      @event = Event.find(params[:id])
+      @event = Event.find_by(slug: params[:id])
     end
 
     # Only allow a list of trusted parameters through.
