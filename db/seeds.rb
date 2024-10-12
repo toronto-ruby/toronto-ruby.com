6d5e7b1ad68bd7e6af89a0069a36b60510bc7b34
crespire <hello@crespire.dev>
2024-10-11 21:34:39 -0400

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,7 @@ events = [
     location: "Workplace One\n51 Wolseley St, Toronto ON\nLower level, enter through doors on Wolseley St.",
     rsvp_link: nil,
     status: :published,
-    presentation: "Integer#even?\nGoing stupidly deep on pointless performance questions, but hopefully learning something fun along the way",
-    presenter: 'Matt Eagar',
+    description: "Integer#even?\nGoing stupidly deep on pointless performance questions, but hopefully learning something fun along the way\nby Matt Eagar",
     sponsor: 'Switch Growth',
     sponsor_logo: 'switch.png',
     sponsor_link: 'https://switchgrowth.com/',
@@ -27,8 +26,7 @@ events = [
     location: "Loop Financial\n500-410 Adelaide Street West, Toronto, ON M5V 1S8",
     rsvp_link: nil,
     status: :published,
-    presentation: 'Rails and the Ruby Garbage Collector: How to Speed Up Your Rails App',
-    presenter: 'Peter Zhu, Senior Developer at Shopify',
+    description: 'Rails and the Ruby Garbage Collector: How to Speed Up Your Rails App by Peter Zhu, Senior Developer at Shopify',
     sponsor: 'Loop Financial',
     sponsor_logo: 'loop.png',
     sponsor_link: 'https://bankonloop.com/',
@@ -39,8 +37,7 @@ events = [
     location: "Workplace One\n51 Wolseley St, Toronto ON\nLower level, enter through doors on Wolseley St.",
     rsvp_link: nil,
     status: :published,
-    presentation: 'All About the Ruby LSP',
-    presenter: 'Vinicius Stock, Staff Developer at Shopify',
+    description: 'All About the Ruby LSP by Vinicius Stock, Staff Developer at Shopify',
     sponsor: 'Switch Growth',
     sponsor_logo: 'switch.png',
     sponsor_link: 'https://switchgrowth.com/',
@@ -52,8 +49,7 @@ events = [
     location: "FinanceIt @ The Well\n8 Spadina Ave\nSuite 2400\nToronto, ON  M5V 2H6\nPlease note that public access to the building is restricted at 7pm! Please arrive before then.\nWhen you arrive at The Well, please take the high rise elevators (19-38) to the 214th floor to get to reception.",
     rsvp_link: nil,
     status: :published,
-    presentation: 'Using Rails introspection to supercharge your editor',
-    presenter: 'Andy Waite, Senior Developer at Shopify',
+    description: 'Using Rails introspection to supercharge your editor by Andy Waite, Senior Developer at Shopify',
     sponsor: 'Financeit',
     sponsor_logo: 'financeit.png',
     sponsor_link: 'https://www.financeit.io/',
@@ -64,8 +60,7 @@ events = [
     location: "Workplace One\n51 Wolseley St, Toronto ON\nLower level, enter through doors on Wolseley St.",
     rsvp_link: 'https://radius.to/groups/toronto-ruby/events/pkgvym5ecyuc',
     status: :published,
-    presentation: 'Concurrency in Ruby',
-    presenter: 'Raj Kumart, Director of Software Engineering at Stealth',
+    description: 'Concurrency in Ruby by Raj Kumart, Director of Software Engineering at Stealth',
     sponsor: 'Switch Growth',
     sponsor_logo: 'switch.png',
     sponsor_link: 'https://switchgrowth.com/',
@@ -76,8 +71,7 @@ events = [
     location: "Workplace One\n51 Wolseley St, Toronto ON\nLower level, enter through doors on Wolseley St.",
     rsvp_link: 'https://radius.to/groups/toronto-ruby/events/s1tczn2usqf5',
     status: :published,
-    presentation: 'Creating A Dev Container Ecosystem For Rails 8',
-    presenter: 'Andrew Novoselac, Senior Software Developer at Shopify',
+    description: 'Creating A Dev Container Ecosystem For Rails 8 by Andrew Novoselac, Senior Software Developer at Shopify',
     sponsor: 'Switch Growth',
     sponsor_logo: 'switch.png',
     sponsor_link: 'https://switchgrowth.com/',
@@ -86,5 +80,8 @@ events = [
 ]
 
 events.each do |event|
-  Event.find_or_create_by!(event)
+  e = Event.find_or_initialize_by(event.except(:location, :description))
+  e.location = event[:location]
+  e.description = event[:description]
+  e.save!
 end
